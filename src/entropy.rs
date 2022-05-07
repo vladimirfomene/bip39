0b83d35319f2e4fcff96a38da8ffde1febc25edd
Vladimir Fomene <vladimirfomene@gmail.com>
2022-05-07 10:35:25 +0000

    create entropy struct and generate new entropy

---
+++ src/entropy.rs
@@ -0,0 +1,35 @@
+use rand::{self, RngCore};
+
+const MIN_NUM_BITS: usize = 128;
+const MAX_NUM_BITS: usize = 256;
+const ENTROPY_MULTIPLE: usize = 32;
+const BYTE_SIZE: usize = 8;
+
+#[derive(Debug)]
+pub struct Entropy{
+    entropy: Vec<u8>
+}
+
+impl Entropy {
+
+    //create new entropy (large random number)
+    //entropy size here is in bits.
+    pub fn new(size: usize) -> Entropy {
+        //Probably change this to a better error handling mechanism if this has 
+        //to be a library.
+
+        //check if size is between 128 and 256
+        assert!((size >= MIN_NUM_BITS) && (size <= MAX_NUM_BITS));
+
+        //check if size is a multiple of 32
+        assert!((size % ENTROPY_MULTIPLE) == 0);
+        let mut entropy: Vec<u8> = vec![0u8; size / BYTE_SIZE];
+        //write better error handling code for this
+        rand::rngs::OsRng.try_fill_bytes(&mut entropy).unwrap();
+        
+
+        return Entropy {
+            entropy
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+use entropy::Entropy;
+
+pub mod entropy;
+
 fn main() {
-    println!("Hello, world!");
+    
 
     //generate entropy (large random number)
+    let ent = entropy::Entropy::new(256);
+    println!("{:?}", ent);
+
+    //generate checksum from entropy
     //Build mnemonic from entropy and wordlist
     //Generate seed from mnemonic using PBKDF2
+}
+
+
+fn generate_checksum(ent: Entropy) {
+    //generate entropy from entropy
 }
